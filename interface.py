2c42352b850b994bdafffb4df8cde80a0b0b17e9
Adam Lassiter <atl35@bath.ac.uk>
2016-03-11 02:01:02 +0000

    Updated crypto's todo list. Beautified all modules.

--- interface.py
+++ interface.py
@@ -6,45 +6,42 @@ import gossamer
 
 class io:
 
-	from sys import stdout
-	try:
-		from msvcrt import getch
-	except ImportError:
-		from curses import getch
+    from sys import stdout
+    try:
+        from msvcrt import getch
+    except ImportError:
+        from curses import getch
 
-	def __init__(self):
-		self.input_str = ""
-		self.prompt = ">"
+    def __init__(self):
+        self.input_str = ""
+        self.prompt = ">"
 
+    def read(self):
+        stdout.write(self.prompt)
+        while True:
+            input_chr = getch()
+            if input_chr == chr(8):  # Backspace
+                if self.input_str:
+                    stdout.write(input_chr)
+                    stdout.write(" ")
+                    stdout.write(input_chr)
+                elif input_chr == chr(13):  # Enter
+                    stdout.write(input_chr)
+                    break
+                elif 32 <= ord(input_chr):  # Printable character
+                    self.input_str += input_chr
+                    stdout.write(input_chr)
+        string, self.input_str = input_str, ""
+        return string
 
-	def read(self):
-		stdout.write(self.prompt)
-		while True:
-			input_chr = getch()
-			if input_chr == chr(8): # Backspace
-				if self.input_str:
-					stdout.write(input_chr)
-					stdout.write(" ")
-					stdout.write(input_chr)
-				elif input_chr == chr(13): # Enter
-					stdout.write(input_chr)
-					break
-				elif 32 <= ord(input_chr): # Printable character
-					self.input_str += input_chr
-					stdout.write(input_chr)
-		string, self.input_str = input_str, ""
-		return string
-
-
-	def write(self, string):
-		from sys import stdout
-		length = len(self.input_str) + len(self.prompt)
-		stdout.write(chr(8)*length + chr(32)*length + chr(8)*length)
-		stdout.write(string + "\n")
-		stdout.write(self.prompt + self.input_str)
-
+    def write(self, string):
+        from sys import stdout
+        length = len(self.input_str) + len(self.prompt)
+        stdout.write(chr(8) * length + chr(32) * length + chr(8) * length)
+        stdout.write(string + "\n")
+        stdout.write(self.prompt + self.input_str)
 
 
 class Program:
 
-	pass
+    pass
