dc2ea969f37232bcbc5cef11b4a3594806eee9c6
Adam Lassiter <atl35@bath.ac.uk>
2016-08-13 21:48:44 +0100

    Added concurrency decorators

---
+++ concurrency.py
@@ -0,0 +1,47 @@
+from functools import wraps
+
+
+def threaded(func):
+    from threading import Thread
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        thread = Thread(target=func, args=args, kwargs=kwargs)
+        thread.start()
+        return thread
+
+    return wrapper
+
+
+def parallelized(func):
+    from multiprocessing import Process
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        process = Process(target=func, args=args, kwargs=kwargs)
+        process.start()
+        return process
+
+    return wrapper
+
+
+def synchronized(lock):
+
+    def locked(func):
+        # from threading import Lock
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            lock.acquire()
+            try:
+                return func(*args, **kwargs)
+            finally:
+                lock.release()
+
+        return wrapper
+
+    return locked
+
+
+def clustered(func):
+    pass
