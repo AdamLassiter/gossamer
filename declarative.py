262b8de8df092d14f5fc7f9ca87495030c31418a
Adam Lassiter <atl35@bath.ac.uk>
2016-09-11 23:26:13 +0100

    Let's call this a bugchange 'cuse this will never be fixed

--- declarative.py
+++ declarative.py
@@ -4,12 +4,13 @@ from sys import stderr
 NONE = 0
 WEAK = 1
 STRONG = 2
-DEFAULT = WEAK
+DEFAULT = STRONG
 
 
 def info(func, action, actual, expected):
-    return "%s %s %s, but expected %s" % \
-        (func.__name__, action, actual, expected)
+    return "%s%s %s %s, but expected %s" % \
+        (func.im_class + "." if hasattr(func, "im_class") else "", func.__name__,
+         action, actual, expected)
 
 
 def accepts(*types, **kwtypes):
@@ -19,25 +20,30 @@ def accepts(*types, **kwtypes):
         def type(obj):
             return obj.__class__
 
+        def pair_type(pair):
+            return (pair[0], type(pair[1])) if pair[1] is not None else None
+
         @wraps(func)
         def wrapper(*args, **kwargs):
             if strength is not NONE:
-                if func.__name__ in dir(args[0]):
+                if args and func.__name__ in dir(args[0]):
                     self, args = args[:1], args[1:]
                 else:
                     self = None
+
                 arg_types = tuple(map(type, args))
-                kwarg_types = set(map(lambda kv: (kv[0], type(kv[1])), kwargs.items()))
-                if arg_types != types or not kwarg_types.issubset(set(kwtypes.items())):
-                    called_types = arg_types + \
-                        tuple(["%s=%s" % kv for kv in kwarg_types])
-                    expected_types = types + \
-                        tuple(["%s=%s" % kv for kv in kwtypes.items()])
-                    msg = info(func, "called with", called_types, types)
+                kwarg_types = set(map(pair_type, kwargs.items()))
+
+                if arg_types != types or not kwarg_types.issubset(set(kwtypes.items() + [None])):
+                    called = arg_types + tuple(["%s=%s" % kv for kv in kwarg_types])
+                    expected = types + tuple(["%s=%s" % kv for kv in kwtypes.items()])
+                    msg = info(func, "called with", called, expected)
+
                     if strength is WEAK:
                         print >> stderr, "Warning: ", msg
                     elif strength is STRONG:
                         raise TypeError(msg)
+
             return func(*(self + args if self else args), **kwargs)
 
         return wrapper
